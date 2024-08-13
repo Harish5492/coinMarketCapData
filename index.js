7fe9a2b3688a4d74e7c473526f09c1ff7223be29
HarishRana <harish.rana@antiersolutions.com>
2024-08-13 13:11:26 +0530

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import fetch from 'node-fetch';
-import express from 'express';
+import fetch from 'node-fetch'
+import express from 'express'
 import cors from 'cors';
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const apikey = '2ad9f937-58a4-458b-8613-72a2dc7350ef'; // Replace with your CoinMarketCap API key
-
-// Enable CORS for all origins
 app.use(cors());
 
 const getListing = async (filter) => {
