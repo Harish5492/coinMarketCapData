3b84307214505a9b75f9a4206a495c63d798b7a2
HarishRana <harish.rana@antiersolutions.com>
2024-08-13 13:08:28 +0530

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import fetch from 'node-fetch'
-import express from 'express'
+import fetch from 'node-fetch';
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const apikey = '2ad9f937-58a4-458b-8613-72a2dc7350ef'; // Replace with your CoinMarketCap API key
+
+// Enable CORS for all origins
+app.use(cors());
 
 const getListing = async (filter) => {
   let url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${apikey}`;
